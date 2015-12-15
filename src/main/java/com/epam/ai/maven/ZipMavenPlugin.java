f5e694d3894b948b0505d53fdde253596c1cb8d8
Alexander <alexander.ivanov.1989@gmail.com>
2015-12-15 10:56:20 +0200

    fixed incorrect file filter

--- src/main/java/com/epam/ai/maven/ZipMavenPlugin.java
+++ src/main/java/com/epam/ai/maven/ZipMavenPlugin.java
@@ -8,21 +8,17 @@ import org.apache.maven.project.MavenProject;
 import org.zeroturnaround.zip.ZipUtil;
 
 import java.io.File;
-import java.util.Map;
 import java.util.stream.Stream;
 
 @Mojo(name = "zip")
 public class ZipMavenPlugin extends AbstractMojo {
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		Map context = getPluginContext();
-		//PluginDescriptor pluginDescriptor = (PluginDescriptor) context.get("pluginDescriptor");
-		MavenProject project = (MavenProject) context.get("project");
-
+		//PluginDescriptor pluginDescriptor = (PluginDescriptor) getPluginContext().get("pluginDescriptor");
+		MavenProject project = (MavenProject) getPluginContext().get("project");
 		String finalName = project.getBuild().getFinalName();
 		String target = project.getBuild().getDirectory();
-		File[] files = new File(target).listFiles((dir, name) -> name.startsWith(finalName));
-
+		File[] files = new File(target).listFiles((dir, name) -> name.startsWith(finalName) && name.endsWith("jar"));
 		Stream.of(files).forEach((file) -> ZipUtil.packEntry(file, new File(target + File.separator + finalName + ".zip")));
 	}
 }
